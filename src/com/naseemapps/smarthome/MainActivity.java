7215db3691c95f9a2c9b4064dc709831f700cd53
Naseem Abuata <naseem.a@live.com>
2013-11-01 21:09:32 +0200

    test4

--- src/com/naseemapps/smarthome/MainActivity.java
+++ src/com/naseemapps/smarthome/MainActivity.java
@@ -6,7 +6,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.HttpURLConnection; 
-import java.net.URL;   
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
